7c8959b20962b1d9bf4008b86c264faf4ccfbd7e
Salomão Junio Santos Batista <salomaojunio@hotmail.com>
2021-06-17 18:53:54 -0300

    First Change
    First Change

--- src/main/java/one/digitalinnovation/aulaprojeto07/Aulaprojeto07Application.java
+++ src/main/java/one/digitalinnovation/aulaprojeto07/Aulaprojeto07Application.java
@@ -8,6 +8,21 @@ public class Aulaprojeto07Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aulaprojeto07Application.class, args);
+		//Working with Arrays
+		int[] arrayOne = {1,2,3};
+		int[] arrayTwo = new int[3];
+
+		for(int i=0;i<3;i++){
+			arrayTwo[i]=i+1;
+		}
+
+		boolean trueOrFalse = arrayOne.equals(arrayTwo);
+		System.out.println(trueOrFalse);
+		
+
 	}
+
+
+
 
 }
